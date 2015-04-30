ae8dd93c9a9f95799f2d0d7d86ffe20b2493e5ab
Erko Bridee <erko.bridee@gmail.com>
2015-04-30 01:33:01 -0300

    gulp style task reviewed #3

--- tools/gulp/tasks/styles.js
+++ tools/gulp/tasks/styles.js
@@ -1,24 +1,8 @@
 module.exports = function(gulp, $) {
 
-  gulp.task('less', function() {
-    $.log("Building css files...");
-    return $.streams.less();
-  });
-
-  /*
-  gulp.task('sass', function() {
-    $.log("Building css files...");
-    return $.streams.sass();
-  });
-  */
-
   gulp.task('styles', function() {
     $.log("Building css files...");
-    if( $.is.sass ) {
-      // return $.streams.sass();
-    } else {
-      return $.streams.less();
-    }
+    return $.streams.less();
   });
 
 };
