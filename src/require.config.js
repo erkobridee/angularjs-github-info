2ebcc5589128e15fe5682145e1fa232cd2b606d9
Erko Bridee <erko.bridee@gmail.com>
2015-05-27 23:03:05 -0300

    angular updated to v1.4.0

--- src/require.config.js
+++ src/require.config.js
@@ -4,26 +4,18 @@ require({
   paths: {
 
     jquery: [
-      // '//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min',
-      // '//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min',
       'vendor/jquery/jquery.min'
     ],
     bootstrap: [
-      // '//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min',
-      // '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.3/js/bootstrap.min',
       'vendor/bootstrap/js/bootstrap.min'
     ],
     angular: [
-      // 'http://code.angularjs.org/1.3.15/angular.min',
-      // '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular.min',
       'vendor/angular/angular.min'
     ],
     angularResource: [
-      // 'http://code.angularjs.org/1.3.15/angular-resource.min',
       'vendor/angular-resource/angular-resource.min'
     ],
     angularRoute: [
-      // 'http://code.angularjs.org/1.3.15/angular-route.min',
       'vendor/angular-route/angular-route.min'
     ]
 
